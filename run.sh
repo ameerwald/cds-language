fe31cbd620f70ffd67c5feb50c185b104cb8efdf
ameerwald <anja.meerwald@gmail.com>
2023-03-08 11:53:40 +0100

    session 6 notes

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#activate virtual environment
+source ./spacy_env/bin/activate
+
+# run the code
+python3 src/new_script.py
+
+# deactive the venv
+deactivate
--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+# create virtual environment
+python3 -m venv spacy_env
+
+#activate virtual environment
+source ./spacy_env/bin/activate
+
+# then install requirements
+python3 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
+python -m spacy download en_core_web_md
+
